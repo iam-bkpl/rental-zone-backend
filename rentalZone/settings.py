7fb188c249d78192720eb469098876ccb16c53c2
iam-bkpl <kapil.bhandari@apexcollege.edu.np>
2022-07-26 14:41:16 +0545

    start admin customization

--- rentalZone/settings.py
+++ rentalZone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    # '/var/www/static/',
+    os.path.join(BASE_DIR,'static')
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- rentalZone/urls.py
+++ rentalZone/urls.py
@@ -16,10 +16,22 @@ Including another URLconf
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings  
+from django.conf.urls.static import static
+from rooms.admin import room_site
+
+
+admin.site.site_header = "Rental Zone"
+admin.site.site_title = "Welcome to Rental Zone Admin Pannel"
+admin.site.index_title = "Welcome to Rental Portal"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rentalAdmin/',room_site.urls),
     path('',include('rooms.urls')),
     path('accounts/',include('accounts.urls'))
     
 ]
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
